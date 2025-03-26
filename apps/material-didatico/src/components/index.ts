a2860e5f35dc44f2e370e1cfec87d0e51ddebe57
Usuario Exemplo <usuario@exemplo.com>
2025-03-26 02:52:53 -0300

    Refactor(components): remove obsolete exports and update index.ts

--- apps/material-didatico/src/components/index.ts
+++ apps/material-didatico/src/components/index.ts
@@ -1,41 +1,24 @@
-// Layout
-export { default as Layout } from './layout/Layout'
-export { Header } from './layout/Header'
-export { Sidebar } from './layout/Sidebar'
-
 // Editor
-export { EditorToolbar } from './editor/EditorToolbar'
-export { BlockSelector } from './editor/BlockSelector'
-export { TextBlock } from './editor/TextBlock'
-export { VideoBlock } from './editor/VideoBlock'
-export { QuizBlock } from './editor/QuizBlock'
-export { ActivityBlock } from './editor/ActivityBlock'
+export * from './editor/Editor'
+export * from './editor/EditorToolbar'
+export * from './editor/BlockSelector'
+export * from './editor/blocks/TextBlock'
+export * from './editor/blocks/VideoBlock'
+export * from './editor/blocks/QuizBlock'
+export * from './editor/blocks/ActivityBlock'
+export * from './editor/blocks/SimulationBlock'
+export * from './editor/blocks/LinkBlock'
 
 // Dashboard
-export { StatsCard } from './dashboard/StatsCard'
-export { RecentActivity } from './dashboard/RecentActivity'
-export { ProgressChart } from './dashboard/ProgressChart'
+export * from './dashboard/ProgressChart'
 
 // Cursos
-export { CourseCard } from './cursos/CourseCard'
-export { CourseForm } from './cursos/CourseForm'
-export { CourseList } from './cursos/CourseList'
-export { CourseStatus } from './cursos/CourseStatus'
+export * from './cursos/CourseCard'
+export * from './cursos/CourseForm'
 
 // Disciplinas
-export { DisciplineCard } from './disciplinas/DisciplineCard'
-export { DisciplineForm } from './disciplinas/DisciplineForm'
-export { DisciplineList } from './disciplinas/DisciplineList'
-export { ModuleTree } from './disciplinas/ModuleTree'
+export * from './disciplinas/DisciplineList'
 
-// Templates
-export { TemplateCard } from './templates/TemplateCard'
-export { TemplateForm } from './templates/TemplateForm'
-export { TemplateList } from './templates/TemplateList'
-export { TemplatePreview } from './templates/TemplatePreview'
-
-// Shared
-export { StatusBadge } from './shared/StatusBadge'
-export { AuthorAvatar } from './shared/AuthorAvatar'
-export { ActionButton } from './shared/ActionButton'
-export { ConfirmationDialog } from './shared/ConfirmationDialog' 
+// Layout
+export * from './layout/Layout'
+export * from './layout/Header' 
