f3fa2fbaf52041be23569945d4adcd36214bc2f3
Usuario Exemplo <usuario@exemplo.com>
2025-03-26 03:16:31 -0300

    Docs(design-system): documentação sobre compartilhamento de componentes

--- apps/material-didatico/src/components/index.ts
+++ apps/material-didatico/src/components/index.ts
@@ -1,24 +1,41 @@
+// Layout
+export { default as Layout } from './layout/Layout'
+export { Header } from './layout/Header'
+export { Sidebar } from './layout/Sidebar'
+
 // Editor
-export * from './editor/Editor'
-export * from './editor/EditorToolbar'
-export * from './editor/BlockSelector'
-export * from './editor/blocks/TextBlock'
-export * from './editor/blocks/VideoBlock'
-export * from './editor/blocks/QuizBlock'
-export * from './editor/blocks/ActivityBlock'
-export * from './editor/blocks/SimulationBlock'
-export * from './editor/blocks/LinkBlock'
+export { EditorToolbar } from './editor/EditorToolbar'
+export { BlockSelector } from './editor/BlockSelector'
+export { TextBlock } from './editor/TextBlock'
+export { VideoBlock } from './editor/VideoBlock'
+export { QuizBlock } from './editor/QuizBlock'
+export { ActivityBlock } from './editor/ActivityBlock'
 
 // Dashboard
-export * from './dashboard/ProgressChart'
+export { StatsCard } from './dashboard/StatsCard'
+export { RecentActivity } from './dashboard/RecentActivity'
+export { ProgressChart } from './dashboard/ProgressChart'
 
 // Cursos
-export * from './cursos/CourseCard'
-export * from './cursos/CourseForm'
+export { CourseCard } from './cursos/CourseCard'
+export { CourseForm } from './cursos/CourseForm'
+export { CourseList } from './cursos/CourseList'
+export { CourseStatus } from './cursos/CourseStatus'
 
 // Disciplinas
-export * from './disciplinas/DisciplineList'
+export { DisciplineCard } from './disciplinas/DisciplineCard'
+export { DisciplineForm } from './disciplinas/DisciplineForm'
+export { DisciplineList } from './disciplinas/DisciplineList'
+export { ModuleTree } from './disciplinas/ModuleTree'
 
-// Layout
-export * from './layout/Layout'
-export * from './layout/Header' 
+// Templates
+export { TemplateCard } from './templates/TemplateCard'
+export { TemplateForm } from './templates/TemplateForm'
+export { TemplateList } from './templates/TemplateList'
+export { TemplatePreview } from './templates/TemplatePreview'
+
+// Shared
+export { StatusBadge } from './shared/StatusBadge'
+export { AuthorAvatar } from './shared/AuthorAvatar'
+export { ActionButton } from './shared/ActionButton'
+export { ConfirmationDialog } from './shared/ConfirmationDialog' 
--- packages/utils/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './hooks'
-export * from './utils'
-export * from './constants'
-export * from './types' 
